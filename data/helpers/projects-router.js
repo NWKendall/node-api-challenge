ec140a56dd51f3b0d2c1c26979cb2207d63c6a89
nicbongo <nicbongo@protonmail.com>
2020-02-14 13:52:05 -0400

    delete working

--- data/helpers/projects-router.js
+++ data/helpers/projects-router.js
@@ -5,18 +5,73 @@ const router = express.Router();
 
 
 router.get("/", (req, res) => {
-  console.log(req)
-
   projectData
   .get()
     .then(projects => {
-      console.log(projects)
       res.status(200).json(projects)
     })
     .catch(err => {
       console.log(`this is get all prjects error`, err)
-      res.status(500).json.apply({ error: "SOMETHING WENT WRONG!" })
+      res.status(500).json({ error: "SOMETHING WENT WRONG! WITH SERVER" })
     })
 })
+
+router.post("/", (req, res) => {
+  
+  const { id } = req.params
+  const project = { ...req.body, id: id }
+
+  console.log(`PROJECT`, project)
+
+  if(!project){
+    res.status(400).json({ message: "no project exists"})
+  } else if (!project.name || !project.description){
+    res.status(400).json({ message: "name or description field missing"})
+  } else {
+    projectData
+    .insert(project)
+    .then(newProj => {
+      !newProj ?
+        res.status(404).json({ error: "project with ID doesn't exist"})
+        : res.status(201).json(newProj)
+    })
+    .catch(err => {
+      console.log(`this is error from insert`, err)
+          res.status(500).json({ error: "There was an error while saving the project to the database"})
+    })  
+  }
+})
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const del = { ...req.body, user_id: id}
+  console.log(`DELETING`, del)
+  projectData
+    .remove(id)
+    .then(rem => {
+      !rem ?
+      res.status(404).json({ message: "The project with the specified ID does not exist." })
+        : res.status(200).json(rem)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "The post could not be removed"  })
+    })
+})
+
+
+// function validateProject(req, res, next) {
+//   // validates all POST requests for new user (not new user posts)  
+//   const { id } = req.params;
+//   const project = { ...req.body, user_id: id };  
+//   console.log(`validate project:`, project)
+
+//   !project ? 
+//     res.status(400).json({ message: "missing user data" }) 
+//     : !project.name || !project.description
+//     ? res.status(400).json({ message: "missing required fields" })
+//     : next();
+  
+// }
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log(`this is npm START index.js`)
 const server = require("./server");
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 server.listen(port, () => console.log(`Server in index.js listening on port:`, port))
 
 
